046487ba7246fe792d8ce2b442c12a7495eb9cae
Julia Arizbeth Gomez Gamez <juliagomez19@gmail.com>
2025-03-31 19:25:38 -0700

    Simplifica la configuración para solucionar problemas en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/snake-games/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
+  base: '/snake-games/'
 })
