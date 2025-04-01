13242ddd5ea9eeaa992edc7b4a46a578adb0cb58
Julia Arizbeth Gomez Gamez <juliagomez19@gmail.com>
2025-03-31 19:56:59 -0700

    Updates

---
+++ redirect-script.js
@@ -0,0 +1,13 @@
+// Single Page Apps for GitHub Pages
+// MIT License
+// Modificado para este proyecto específico
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location)) 
